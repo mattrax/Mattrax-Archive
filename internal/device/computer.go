b8da6a6092af38f00c1f882edd0935398870165c
Oscar Beaumont <oscartbeaumont@gmail.com>
2018-07-26 16:39:43 +0100

    More Work On The Device Structs
    Defined Each Of The Device States. More Work For Interfacing The Custom Struct With The Database.

--- internal/device/computer.go
+++ internal/device/computer.go
@@ -5,40 +5,29 @@ import (
   "github.com/go-pg/pg"
 )
 
-/*test := Computer{
-  Name: "Testing Qwerty",
+/* Database Interface */
+type DeviceInformation struct {
+  DisplayName string `sql:"DisplayName"`
+  // Enrollment State (Genericly Designed)
+  // OS Information
+  // /
 }
-fmt.Println(test.Testing())
-fmt.Println(test.GetName())
 
-fmt.Println("")
-
-aPC := WindowsComputer{
-  Computer: Computer{
-    Name: "Testing Qwerty",
-  },
-  Testing123: "This Is A Windows Computer",
-}
-fmt.Println(aPC.Computer.GetName())
-fmt.Println(aPC.Testing123)*/
-
-type WindowsComputer struct {
-    Computer
-    Testing123 string
+type Device struct {
+	TableName struct{} `sql:"devices"`
+	UUID string `sql:"uuid,pk"`
+  DeviceInformation DeviceInformation `sql:"DeviceInformation"`
 }
 
 
-type Device struct { /* TEMP */
-	TableName      struct{}       `sql:"devices"`
-	UUID           string         `sql:"uuid,pk"`
 
-	/*DeviceState    int            `sql:"DeviceState,notnull"`
-	DeviceDetails  DeviceDetails  `sql:"DeviceDetails,notnull"`
-	DeviceTokens   DeviceTokens   `sql:"DeviceTokens,notnull"`
-	DevicePolicies DevicePolicies `sql:"DevicePolicies,notnull"`*/
-}
+
+
+
+
 
 //Use PG .Table("author_books")
+
 /*var devices []Device
 if err := pgdb.Model(&devices).Select(); err != nil {
   fmt.Println(err)
@@ -48,6 +37,14 @@ if err := pgdb.Model(&devices).Select(); err != nil {
 for _, device := range devices {
   fmt.Println(device.UDID)
 }*/
+
+
+
+
+type WindowsComputer struct {
+    Computer
+    Testing123 string
+}
 
 func main() {
   pgdb := pg.Connect(&pg.Options{
@@ -61,17 +58,16 @@ func main() {
 		fmt.Println(err)
     return
 	}
-  fmt.Println(device.UUID)
 
+  // Create The Computer
+  aComputer := WindowsComputer{}
+  aComputer.LoadDatabaseInformation(device)
 
+  // Display Output
 
+  fmt.Println(aComputer.DisplayName)
 
-
-
-  /*comp := Computer{}
-
-  fmt.Println(comp)
-  fmt.Println(comp.Name)*/
+  //fmt.Println(device.DeviceInformation.DisplayName)
 }
 
 
@@ -82,13 +78,22 @@ func main() {
 
 
 
-
+//Device States
+//  1 = No Device Contact
+//  2 = Enrollment Began
+//  3 = Prestage Enrollment In Progress
+//  4 = Initial Configuration/Applications Loading
+//  5 = Device Complys To Organisation Security/etc Standards
+//  6 = Device Is Enrolled And Healthy
+//  7 = Device Is Enrolled And Not Healthy (Lost Communication Or Something)
+//  8 = Device Is In Recovery/Lockdown
+//  9 = The Device Is In A Bodyless State (The Computers Software is being Reset So The Info Wont Change But The Policys/Apps Will Need to Be Reapplyed)
 
 
 type Computer struct {
-  Name string //`plist:"Name,omitempty"`
-  UUID string //`plist:"uuid,omitempty"`
-  DeviceState int //`plist:"DeviceState,omitempty"`
+  UUID string
+  DisplayName string
+  DeviceState int
 
   //Device Information
   //Network Interfaces
@@ -96,6 +101,11 @@ type Computer struct {
   //Installed Applications -> Maybe Device Specific
 }
 
-func (comp Computer) FillDeviceInformation() string {
-  return "Testing123"
+func (comp *Computer) LoadDatabaseInformation(_comp Device) {
+  comp.DisplayName = _comp.DeviceInformation.DisplayName
+}
+
+func (comp *Computer) Checkout() {
+  //Unenroll For THe Server Side
+  //Maybe Return Error/Nil
 }
